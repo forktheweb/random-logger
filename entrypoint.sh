622a807b2f33372b042b700429dbe5cbb92e99de
Christopher D. Hogan, Cisco Systems <chrhogan@cisco.com>
2019-04-16 13:49:45 -0400

    Modified the fork a bit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 while [ 1 ]
 do
-   waitTime=$(shuf -i 1-5 -n 1)
+   UUID=$(cat /proc/sys/kernel/random/uuid)
+   waitTime=0
    sleep $waitTime &
    wait $!
    instruction=$(shuf -i 0-4 -n 1)
    d=`date -Iseconds`
    case "$instruction" in
-      "1") echo "$d ERROR something happened in this execution."
+      "1") echo "$d ERROR something happened in this execution. $UUID"
       ;;
-      "2") echo "$d INFO takes the value and converts it to string."
+      "2") echo "$d INFO takes the value and converts it to string. $UUID"
       ;;
-      "3") echo "$d WARN variable not in use."
+      "3") echo "$d WARN variable not in use. $UUID"
       ;;
-      "4") echo "$d DEBUG first loop completed."
+      "4") echo "$d DEBUG first loop completed. $UUID"
       ;;
    esac
 done
